6966ad13a36c27f350636dd0a831e8f0ef292378
Olimpo Macea <olimpo3m@gmail.com>
2022-04-20 12:13:17 -0500

    Casi listo para correr a produccion

--- proyectmemes/src/App.js
+++ proyectmemes/src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+//Las imagenes de publicidad en facebook por lo general son de 500 * 500 px
 function App() {
+
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+  
+
+  const onChangeSuperior = function (evento) {
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeInferior = function (evento) {
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeImagen = function (evento) {
+    setImagen(evento.target.value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/*Select picker de memes*/}
+      <select onChange={onChangeImagen}>
+        <option value="llamas">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="raptor">Filosoraptor</option>
+        <option value="pensando">Hombre pensando</option>
+      </select><br />
+
+      {/*Input text - Linea superior del memes*/}
+
+      <input onChange={onChangeSuperior} type="text" placeholder="Texto Superior" /> <br />
+
+      {/*//Input text - Linea inferior del memes*/}
+
+      <input onChange={onChangeInferior} type="text" placeholder="Linea Inferior" /> <br />
+
+      {/*//Boton para descargar el meme*/}
+
+      <button>Exportar</button>
+
+      <div className="meme">
+        <span>{linea1}</span> <br />
+        <span>{linea2}</span>
+        <img src={"/imageneMemes/" +imagen+".jpg"}/>
+      </div>
+
+
     </div>
+
   );
 }
 
